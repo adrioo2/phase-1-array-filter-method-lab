62010fe96ef27499d3adea24003a2e80742c9b6f
Lourdes Quizhpilema <adrianalou42@gmail.com>
2023-05-20 16:44:00 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const findMatching = (names, string) => {
+  return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+const fuzzyMatch = (array, string) => {
+  return array.filter(array => array.charAt(0) === string.charAt(0))
+}
+function matchName(array, string) {
+  return array.filter(function (array) { 
+    return array.name === string })
+}
